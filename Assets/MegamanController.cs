cb6e2df2fd3b5a29a128780407276b8c50a286ea
A. Cody Schuffelen <astronomix@gmail.com>
2014-03-22 21:15:14 -0500

    left and right movement + idle animations + controller

---
+++ Assets/MegamanController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MegamanController : MonoBehaviour {
+
+	//Accessible editor properties
+	public float horizontalForce = 1.0f;
+
+	// Use this for initialization
+	public void Start() {
+		
+	}
+	
+	// Update is called once per frame
+	public void Update() {
+		float force = horizontalForce * Input.GetAxis("Horizontal") * rigidbody2D.mass;
+		rigidbody2D.AddForce(new Vector2(force, 0));
+
+		GetComponent<Animator>().SetFloat("horizontalVelocity", rigidbody2D.velocity.x);
+	}
+}
