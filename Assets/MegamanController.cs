1ea3a8df49ea03ca59ce6dccf51fa01b10452984
A. Cody Schuffelen <astronomix@gmail.com>
2014-03-23 23:12:28 -0500

    Added jumping and made camera follow character

--- Assets/MegamanController.cs
+++ Assets/MegamanController.cs
@@ -5,6 +5,8 @@ public class MegamanController : MonoBehaviour {
 
 	//Accessible editor properties
 	public float horizontalForce = 1.0f;
+	public float jumpForce = 1.0f;
+	public bool canJump = false;
 
 	// Use this for initialization
 	public void Start() {
@@ -13,9 +15,26 @@ public class MegamanController : MonoBehaviour {
 	
 	// Update is called once per frame
 	public void Update() {
-		float force = horizontalForce * Input.GetAxis("Horizontal") * rigidbody2D.mass;
-		rigidbody2D.AddForce(new Vector2(force, 0));
-
 		GetComponent<Animator>().SetFloat("horizontalVelocity", rigidbody2D.velocity.x);
+
+		if(canJump) {
+			if(Input.GetButton("Jump")) {
+				rigidbody2D.AddForce(new Vector2(0, jumpForce * rigidbody2D.mass));
+				canJump = false;
+			}
+
+			float force = horizontalForce * Input.GetAxis("Horizontal") * rigidbody2D.mass;
+			rigidbody2D.AddForce(new Vector2(force, 0));
+		}
+	}
+
+	public void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log("entered collision");
+		canJump = true;
+	}
+
+	public void OnTriggerExit2D(Collider2D other) {
+		Debug.Log("exited collisiion");
+		canJump = false;
 	}
 }
